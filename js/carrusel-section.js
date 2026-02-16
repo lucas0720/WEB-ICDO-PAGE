2b8b7d81b938e3894c7caf2d97788c020a11da31
lukh <lucas.araya.ar@gmail.com>
2026-02-16 00:33:36 -0300

    agregando seccion de carrusel y seccion de cdoh

---
+++ js/carrusel-section.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let track = document.querySelector(".carrusel__list"); 
+    let items = document.querySelectorAll(".item"); 
+    
+    let index = 0; 
+
+    setInterval(function(){
+        index++; 
+        if (index >= items.length) {
+            index = 0;
+        }
+        let porcentaje = index * -100;
+        
+        track.style.transform = "translateX(" + porcentaje + "%)";
+        
+    }, 9000); 
+});
